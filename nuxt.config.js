ac8f87108924090ec1145fd5d2b5aec6bba1d7a4
liuliu <liuliugit@gmail.com>
2020-12-01 08:17:27 +0800

    feat: set nuxt.config.js
    1. use .env file to control title in html header
    2. add description
    3. import script in static folder

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,18 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'NUXT',
+    title: process.env.site_title || 'NUXT',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '前端專案建置、AWS、EC2' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    script: [
+      {src: '/jquery-min-3.5.1.js'},
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
