1bfafa3bcb55dea83664c361ce64213bac95f48b
liuliu <liuliugit@gmail.com>
2020-12-07 08:23:26 +0800

    feat: refactor CourseList component.
    1. create Course component to reuse structure about courses.

---
+++ components/CourseList/Course.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="course_item">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="course.image" :alt="course.title" />
+      </figure>
+    </div>
+    <div class="course_title_parent">
+      <div class="course_title is-clearfix">
+        <h2 class="is-pulled-left">{{ course.title }}</h2>
+      </div>
+    </div>
+    <div class="description">{{ course.description }}</div>
+    <nuxt-link
+      class="details"
+      :to="{
+        name: 'course-id',
+        params: {
+          id: course.id,
+        },
+      }"
+    >
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    course: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "./courseList.scss";
+</style>
--- components/CourseList/CourseList.vue
+++ components/CourseList/CourseList.vue
@@ -6,29 +6,7 @@
         v-for="course in courses"
         :key="course.id"
       >
-        <div class="course_item">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img :src="course.image" :alt="course.title" />
-            </figure>
-          </div>
-          <div class="course_title_parent">
-            <div class="course_title is-clearfix">
-              <h2 class="is-pulled-left">{{ course.title }}</h2>
-            </div>
-          </div>
-          <div class="description">{{ course.description }}</div>
-          <nuxt-link
-            class="details"
-            :to="{
-              name: 'course-id',
-              params: {
-                id: course.id,
-              },
-            }"
-          >
-          </nuxt-link>
-        </div>
+        <Course :course="course" />
       </div>
       <div v-if="courses.length === 0" class="section">
         <p>沒有更多課程了！</p>
@@ -37,8 +15,13 @@
   </div>
 </template>
 <script>
+import Course from "@/components/CourseList/Course";
+
 export default {
   name: "CourseList",
+  components: {
+    Course,
+  },
   data() {
     return {
       courses: [
